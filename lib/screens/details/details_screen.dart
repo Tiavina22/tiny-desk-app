39b63337bb34b5f59c58e784559689d33ed7274d
Tiavina RAMILISON <tiavinaramilison22@gmail.com>
2025-01-26 13:59:32 +0300

    [FEAT] : implements color syntax, for this moment dart implemented only.

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tiny_desk/services/database/database_service.dart';
 
+import 'package:flutter_highlight/flutter_highlight.dart';
+import 'package:flutter_highlight/themes/github.dart';
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
+
 class DetailScreen extends StatefulWidget {
   final Map<String, dynamic> item;
 
@@ -22,7 +26,8 @@ class _DetailScreenState extends State<DetailScreen> {
   void initState() {
     super.initState();
     _titleController = TextEditingController(text: widget.item['title']);
-    _descriptionController = TextEditingController(text: widget.item['description']);
+    _descriptionController =
+        TextEditingController(text: widget.item['description']);
     _contentController = TextEditingController(
         text: widget.item['command'] ??
             widget.item['note'] ??
@@ -50,7 +55,7 @@ class _DetailScreenState extends State<DetailScreen> {
     });
   }
 
-   // Méthode pour copier le contenu dans le presse-papiers
+  // Méthode pour copier le contenu dans le presse-papiers
   void _copyToClipboard() {
     Clipboard.setData(ClipboardData(text: _contentController.text));
     ScaffoldMessenger.of(context).showSnackBar(
@@ -126,15 +131,19 @@ class _DetailScreenState extends State<DetailScreen> {
   Widget build(BuildContext context) {
     final bool isCommand = widget.item.containsKey('command');
 
+    final bool isCode = widget.item.containsKey('code');
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
         foregroundColor: Theme.of(context).colorScheme.onSurface,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new, color: Colors.blue), // Icône personnalisée
+          icon: const Icon(Icons.arrow_back_ios_new,
+              color: Colors.blue), // Icône personnalisée
           onPressed: () {
-            Navigator.pop(context, true); // Retour personnalisé avec un résultat
+            Navigator.pop(
+                context, true); // Retour personnalisé avec un résultat
           },
         ),
         actions: [
@@ -168,7 +177,7 @@ class _DetailScreenState extends State<DetailScreen> {
                 ),
               ),
               const Divider(height: 32.0, thickness: 1.0),
-             // Condition pour afficher le contenu en mode terminal ou normal
+              // Condition pour afficher le contenu en mode terminal ou normal
               if (isCommand)
                 Stack(
                   children: [
@@ -199,7 +208,67 @@ class _DetailScreenState extends State<DetailScreen> {
                       top: 8.0,
                       right: 8.0,
                       child: IconButton(
-                        icon: const Icon(Icons.copy, color: Color.fromARGB(255, 136, 136, 136)),
+                        icon: const Icon(Icons.copy,
+                            color: Color.fromARGB(255, 136, 136, 136)),
+                        onPressed: _copyToClipboard,
+                      ),
+                    ),
+                  ],
+                )
+              else if (isCode)
+                Stack(
+                  children: [
+                    // Affichage du code avec coloration syntaxique
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(0.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: HighlightView(
+                        _contentController.text,
+                        language: 'dart',
+                        theme: monokaiSublimeTheme,
+                        padding: const EdgeInsets.all(12.0),
+                        textStyle: const TextStyle(
+                          fontFamily: 'Courier',
+                          fontSize: 14.0,
+                          height: 1.5,
+                        ),
+                      ),
+                    ),
+
+                    // Champ de texte transparent pour l'édition
+                    Container(
+                      padding: const EdgeInsets.all(12.0),
+                      child: TextField(
+                        controller: _contentController,
+                        style: TextStyle(
+                          fontFamily: 'Courier',
+                          fontSize: 14.0,
+                          height: 1.5,
+                          color: Colors.transparent,
+                        ),
+                        maxLines: null,
+                        decoration: const InputDecoration(
+                          hintText: 'Ajoutez du contenu ici...',
+                          hintStyle: TextStyle(color: Colors.transparent),
+                          border: InputBorder.none,
+                        ),
+                        cursorColor: Colors.white,
+                        onChanged: (text) {
+                          setState(() {});
+                        },
+                      ),
+                    ),
+
+                    // Icône de copie en haut à droite
+                    Positioned(
+                      top: 8.0,
+                      right: 8.0,
+                      child: IconButton(
+                        icon: const Icon(Icons.copy,
+                            color: Color.fromARGB(255, 136, 136, 136)),
                         onPressed: _copyToClipboard,
                       ),
                     ),
